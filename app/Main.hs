327d58526500b9a6365c7d0e3c50bbd4fa29e703
Tirtharaj Pramanik <tbytes404@icloud.com>
2025-06-30 12:52:30 +0530

    syntax change

--- app/Main.hs
+++ app/Main.hs
@@ -2,29 +2,26 @@ module Main where
 import Data.Maybe (fromMaybe, catMaybes)
 import Data.Char (toLower)
 import System.IO (hPutStrLn, stderr)
-import Data.Map (Map)
 import qualified Data.Map as Map
 
-type EmojiMap = Map String String
+type EmojiMap = Map.Map String String
 
 tmoji :: EmojiMap -> String -> String
 tmoji emojiMap text = unlines . map (unwords . map eval . words) . lines $ text
   where
-
   eval :: String -> String
   eval tag = case tag of
     ':':word -> emojiFor word
     '@':word -> let emoji = emojiFor word in
-      if emoji == word then emoji else word ++ emoji
+      word ++ if emoji == word then "" else emoji
     _ -> tag
 
   emojiFor :: String -> String
   emojiFor word = fromMaybe word $ Map.lookup (map toLower word) emojiMap
 
 mapTmoji :: String -> EmojiMap
-mapTmoji text = Map.fromList . catMaybes $ map parse (lines text)
+mapTmoji text = Map.fromList . catMaybes $ map parse $ lines text
   where
-
   parse :: String -> Maybe (String, String)
   parse line = case words line of
     (tag:emoji:_) -> Just(tag, emoji)
